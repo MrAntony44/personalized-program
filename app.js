071b1827cca89a5b9ec0c57be0db9ddc23419f13
antonislekkas <antonislekkas05@gmail.com>
2022-02-17 23:00:55 +0200

    commit

--- app.js
+++
@@ -1,8 +0,0 @@
-var Excel = require('exceljs');
-var workbook = new Excel.Workbook();
-workbook.xlsx.readFile("test.xlsx")
-    .then(function() {
-        ws = workbook.getWorksheet("Εκπαιδευτικοί")
-        cell = ws.getCell('A1').value
-        console.log(cell)
-    });
